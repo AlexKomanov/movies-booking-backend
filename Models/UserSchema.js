2b0ce2b5f5ff925d69429de85157e041cb448885
AlexKomanov <akomanov88@gmail.com>
2023-11-26 23:02:20 +0200

    Starting with UserSchema

--- Models/UserSchema.js
+++ Models/UserSchema.js
@@ -1,9 +1,37 @@
 const momgoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const userSchema = new momgoose.Schema({}, {
+const userSchema = new momgoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    bookings:{
+        type: Array,
+        default: [],
+    }
+}, {
     timestamps: true
 })
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+
+    next();
+});
 
 const User = momgoose.model('User', userSchema);
 module.exports = User;
--- Routes/Auth.js
+++ Routes/Auth.js
@@ -13,23 +13,65 @@ router.get('/test', async (req, res) => {
     })
 })
 
-function createResponse(isOk, message, data) {
+function createResponse(ok, message, data) {
     return {
-        isOk,
+        ok,
         message,
         data,
     };
 }
 
-router.post('/register', async (req, res, next) => {})
+router.post('/register', async (req, res, next) => {
+    try {
+        const { name, email, password} = req.body;
+        const existingUser = await User.findOne({email: email})
 
-router.post('/login', async (req, res, next) => {})
+        if(existingUser) {
+            return res.status(409).json(createResponse(false, 'This Email already exists'));
+        }
+
+        const newUser = new User({
+            name, password, email
+        });
+        
+        await newUser.save();
+        res.status(201).json(createResponse(true, "Registration Completed Successfully!"))
+    } 
+    catch (error) {
+        next(error);
+    }
+})
+
+router.post('/login', async (req, res, next) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+    
+    if (!user) {
+        return res.status(400).json(createResponse(false, 'Invalid credentials'));
+    }
+
+    const isMatch = await bcrypt.compare(password, user?.password);
+    if (!isMatch) {
+        return res.status(400).json(createResponse(false, 'Invalid credentials'));
+    }
+
+    const authToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '10m' });
+    const refreshToken = jwt.sign({ userId: user._id }, process.env.JWT_REFRESH_SECRET_KEY, { expiresIn: '30m' });
+    res.cookie('authToken', authToken,  { httpOnly: true, secure: true, sameSite: 'None' });
+    res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, sameSite: 'None' });
+
+    res.status(200).json(createResponse(true, 'Login successful', {
+        authToken,
+        refreshToken
+    }));
+
+})
 
 router.post('/sendotp', async (req, res, next) => {})
 
 router.get('/checklogin', async (req, res, next) => {})
 
 
-// router.use(errorHandler);
+router.use(errorHandler);
 
 module.exports = router;
